45eb88589a7e31fe186dc022e7ba64bec685f8b7
Faizan Rao <68699819+Faizan-Rao@users.noreply.github.com>
2023-10-26 11:18:54 +0500

    Chapter 2 completed

---
+++ Ch 2 Code Quality/2.1.1 test.js
@@ -0,0 +1,37 @@
+// ============== 1 Test x raise to the power ==============
+// describe("pow", function() {
+//               describe("raises x to power 3", function() {
+                        
+//                       function makeTest(x) {
+//                               let expected = x * x * x;
+//                               it(`${x} raises to power of ${expected}`, function() {
+//                         //       to(" raises to power of ${expected}", function(){
+//                                       assert.equal(pow(x,3), expected)
+                                      
+//                               })
+//                       }
+      
+//                       for(let i = 1; i<= 10; i++)
+//                       {
+//                               makeTest(i)
+//                       }
+//               })             
+//         })
+
+// ============  2 Testing Task =============================
+// describe('pow', ()=>{
+
+//         describe("Raises x to the power n", function() {
+//                 let x = 5;
+              
+//                 it("Raises x to the power 1", function() {
+//                         assert.equal(pow(x, 1), 5);
+//                 })
+//                 it("Raises x to the power 2", function() {
+//                         assert.equal(pow(x, 2), 25);
+//                 })
+//                 it("Raises x to the power 3", function() {
+//                         assert.equal(pow(x, 3), 125);
+//                 })
+//               });
+// })
